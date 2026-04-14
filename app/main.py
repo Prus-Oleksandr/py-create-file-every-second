d06ef3e18609af13a0602376ed21134577c1f614
cheep <cheepaxxa2003@gmail.com>
2026-04-14 20:06:11 +0300

    Implement main loop with datetime logging and sleep

--- app/main.py
+++ app/main.py
@@ -1,10 +1,22 @@
-from datetime import datetime  # DO NOT CHANGE THIS IMPORT
+from datetime import datetime
+from time import sleep
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        now = datetime.now()
+        filename = f"app-{now.hour}_{now.minute}_{now.second}.log"
+        file_content = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        with open(filename, "w") as new_file:
+            new_file.write(file_content)
+
+        print(f"{file_content} {filename}")
+        sleep(1)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        pass
